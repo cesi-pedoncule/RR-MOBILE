b3ccb29435a525e2173d149f24b04e92c0ff9047
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-25 19:26:57 +0100

    Adding custom style on button component

--- src/components/InputButton.tsx
+++ src/components/InputButton.tsx
@@ -5,11 +5,12 @@ import commonStyles from '../styles/commonStyles';
 interface Props {
   label: string;
   callBack: ()=>void;
+  style?: any;
 }
 
-export default function InputButton({ label, callBack}: Props) {
+export default function InputButton({ label, callBack, style}: Props) {
   return (
-    <TouchableHighlight style={commonStyles.btnBackground} onPress={callBack} underlayColor={"#000000"}>
+    <TouchableHighlight style={[commonStyles.btnBackground, style]} onPress={callBack} underlayColor={"#000000"}>
         <Text style={commonStyles.btnText}>{label}</Text>
     </TouchableHighlight>
   )
