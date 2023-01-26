facbc1f5d8134e9e18114e4a68c5807adbeffe0c
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-26 17:09:24 +0100

    Removing usless content #2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 import commonStyles from "../styles/commonStyles";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
 import HomeButton from "./HomeButton";
 
 interface Props {
     label: string;
 }
 
-type LoginStackParamList = {
-    Home: undefined;
-};
-
 export default function Header({label}: Props) {
-    const navigation = useNavigation<StackNavigationProp<LoginStackParamList>>();
-
-    const onPressIcon = () => {
-        navigation.navigate('Home');
-    } 
 
     return (
         <View style={commonStyles.header}>
