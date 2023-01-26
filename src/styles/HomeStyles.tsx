b9cd4ffd5d32593f664472797bf5b9b1eac75b4a
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-26 14:24:07 +0100

    Adding Components & styles for home screen

---
+++ src/styles/HomeStyles.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    resourcesContainer: {
+        marginTop: '7vh',
+        marginBottom: '3vh',
+        alignItems: 'center',
+    },
+});
