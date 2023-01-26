1b9c1256a49d65f32058fc1b11b29cb60d40487f
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-26 17:06:05 +0100

    Adding Home Button and searchBar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+import TopbarStyles from "../styles/TopbarStyles";
+
+export default function SearchBar() {
+    return (
+        <View style={TopbarStyles.searchBarBackground}>
+            <TextInput placeholder='Rechercher' style={TopbarStyles.searchBar} />
+        </View>
+    );
+}
