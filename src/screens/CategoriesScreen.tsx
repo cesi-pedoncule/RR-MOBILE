064fc08b7ceff9ff326264ed33d52aea9824098b
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-27 15:39:19 +0100

    Add dynamic categories from lib

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -8,45 +8,38 @@ import TopBar from "../components/TopBar";
 import CategoryCard from "../components/CategoryCard";
 
 export default function CategoriesScreen({ route }: any) {
-  const client = route.params as Client;
-  const [showMoreItems, setShowMoreItems] = useState(false);
+    const client = route.params as Client;
+    const [showMoreItems, setShowMoreItems] = useState(false);
+    const [categories, setCategories] = useState(Array.from(client.categories.cache.values()));
 
-  const onClickShowMoreItems = () => {
-      setShowMoreItems(true);
-      alert('Load more items');
-  }
+    const onClickShowMoreItems = () => {
+        setShowMoreItems(true);
+    }
   
-  return (
-    <View style={commonStyles.container}>
-		<TopBar/>
-		<View style={commonStyles.contentWithTopBar}> 
-			<ScrollView>
-        <View style={commonStyles.categoriesContainer}>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
-          {
-            !showMoreItems ?
-            <ButtonShowMoreItems callBack={onClickShowMoreItems} />
-            : null
-          }
+    return (
+        <View style={commonStyles.container}>
+            <TopBar/>
+            <View style={commonStyles.contentWithTopBar}> 
+                <ScrollView>
+                    <View style={commonStyles.categoriesContainer}>
+                        {
+                            categories.map((category, i) => {
+                                if (!showMoreItems && i < 6) {
+                                    return <CategoryCard title={category.name} numberResource={category.resources.size}></CategoryCard>
+                                } else if (showMoreItems) {
+                                    return <CategoryCard title={category.name} numberResource={category.resources.size}></CategoryCard>
+                                }
+                            })
+                        }
+                        {
+                            !showMoreItems ?
+                            <ButtonShowMoreItems callBack={onClickShowMoreItems} />
+                            : null
+                        }
+                    </View>
+                </ScrollView>
+                <NavBar client={client}/>
+            </View>
         </View>
-      </ScrollView>
-			<NavBar client={client}/>
-		</View>
-	</View>
-  )
+    )
 }
