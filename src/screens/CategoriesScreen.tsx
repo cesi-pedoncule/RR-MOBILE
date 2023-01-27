18b977a26201e839dd76e003967b0dea8ab5157d
HM22-code <H.M-22@outlook.fr>
2023-01-27 13:42:57 +0100

    Add Category screen

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,14 +1,51 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, ScrollView } from 'react-native'
 import NavBar from "../components/NavBar";
 import commonStyles from "../styles/commonStyles";
+import ButtonShowMoreItems from "../components/buttonShowMoreItems";
+import TopBar from "../components/TopBar";
+import CategoryCard from "../components/CategoryCard";
 
 export default function CategoriesScreen() {
+  const [showMoreItems, setShowMoreItems] = useState(false);
+
+  const onClickShowMoreItems = () => {
+      setShowMoreItems(true);
+      alert('Load more items');
+  }
+  
   return (
     <View style={commonStyles.container}>
-      <Text>CategoriesScreen</Text>
-      <NavBar/>
-    </View>
+		<TopBar/>
+		<View style={commonStyles.contentWithTopBar}> 
+			<ScrollView>
+        <View style={commonStyles.categoriesContainer}>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          <CategoryCard title={"Title"} numberResource={0}></CategoryCard>
+          {
+            !showMoreItems ?
+            <ButtonShowMoreItems callBack={onClickShowMoreItems} />
+            : null
+          }
+        </View>
+      </ScrollView>
+			<NavBar/>
+		</View>
+	</View>
   )
 }
