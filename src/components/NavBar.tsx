2e9c4d809f9d0b6677a5ac66f564ee82e0e24a77
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-27 13:18:50 +0100

    Fix bad code of kent1

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,46 +4,46 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 
 type NavBarStackParamList = {
-  Categories: undefined;
-  Profil: undefined;
-  ShareCreate: undefined;
-  Ressources: undefined;
+    Categories: undefined;
+    Profil: undefined;
+    ShareCreate: undefined;
+    Ressources: undefined;
 }
 
 export default function NavBar() {
-  const navigation = useNavigation<StackNavigationProp<NavBarStackParamList>>();
+    const navigation = useNavigation<StackNavigationProp<NavBarStackParamList>>();
 
-  const onClickCategoriesButton = () => {
-      console.log('Categories')
-      navigation.navigate('Categories');
-  }
+    const onClickCategoriesButton = () => {
+        console.log('Categories')
+        navigation.navigate('Categories');
+    }
 
-  const onClickProfilButton = () => {
-      navigation.navigate('Profil');
-  }
+    const onClickProfilButton = () => {
+        navigation.navigate('Profil');
+    }
 
-  const onClickShareResourceButton = () => {
-    navigation.navigate('ShareCreate');
-  }
+    const onClickShareResourceButton = () => {
+        navigation.navigate('ShareCreate');
+    }
 
-  const onClickRessourcesButton = () => {
-    navigation.navigate('Ressources');
-  }
+    const onClickRessourcesButton = () => {
+        navigation.navigate('Ressources');
+    }
 
-  return (
-    <View style={NavBarStyles.container}>
-      <TouchableHighlight onPress={onClickRessourcesButton} style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
-        <Image source={require('../assets/Ressources.png')} style={NavBarStyles.logo}  />
-      </TouchableHighlight>
-      <TouchableHighlight onPress={onClickCategoriesButton}style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
-      <Image source={require('../assets/Catalogue.png')} style={NavBarStyles.logo} />
-      </TouchableHighlight>
-      <TouchableHighlight onPress={onClickShareResourceButton}style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
-        <Image source={require('../assets/Partage.png')} style={NavBarStyles.logo}  />
-      </TouchableHighlight>
-      <TouchableHighlight onPress={onClickProfilButton}style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
-        <Image source={require('../assets/Profil.png')} style={NavBarStyles.logo}  />
-      </TouchableHighlight>
-    </View>
-  )
+    return (
+        <View style={NavBarStyles.container}>
+            <TouchableHighlight onPress={onClickRessourcesButton} style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
+                <Image source={require('../assets/Ressources.png')} style={NavBarStyles.logo}  />
+            </TouchableHighlight>
+            <TouchableHighlight onPress={onClickCategoriesButton}style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
+                <Image source={require('../assets/Catalogue.png')} style={NavBarStyles.logo} />
+            </TouchableHighlight>
+            <TouchableHighlight onPress={onClickShareResourceButton}style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
+                <Image source={require('../assets/Partage.png')} style={NavBarStyles.logo}  />
+            </TouchableHighlight>
+            <TouchableHighlight onPress={onClickProfilButton}style={NavBarStyles.buttonNavBar} underlayColor={"#FFF"}>
+                <Image source={require('../assets/Profil.png')} style={NavBarStyles.logo}  />
+            </TouchableHighlight>
+        </View>
+    )
 }
