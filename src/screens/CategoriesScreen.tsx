e02dcd32a09cf46cb18bb892aecd325cec2fb9eb
HM22-code <H.M-22@outlook.fr>
2023-01-27 13:47:27 +0100

    Remove unused import

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,4 +1,3 @@
-import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
 import { View, ScrollView } from 'react-native'
 import NavBar from "../components/NavBar";
