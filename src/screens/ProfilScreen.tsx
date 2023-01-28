fd6c815b560e7dad9e7f4577ac3f3fd897e2d986
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-28 16:37:47 +0100

    Adding profile datas

--- src/screens/ProfilScreen.tsx
+++ src/screens/ProfilScreen.tsx
@@ -1,15 +1,48 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import React, { useEffect } from "react";
 import { View, Text } from 'react-native'
 import { Client } from "rr-apilib";
+import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import TopBar from "../components/TopBar";
 import commonStyles from "../styles/commonStyles";
+import ProfileStyles from "../styles/ProfileStyles";
+
+type HomeStackParamList = {
+    Home: undefined;
+};
 
 export default function ProfilScreen({route}: any) {
-    const client = route.params as Client;
     
+    const client = route.params as Client;
+    const navigation = useNavigation<StackNavigationProp<HomeStackParamList>>();
+    
+    const user = client.auth.me;
+    const userProfilName = user?.name + ' ' + user?.firstname;
+
+    const checkUserIsConnected = () => {
+        if (!user) {
+            navigation.navigate('Home');
+        }
+    }
+
+    useEffect(() => {
+        checkUserIsConnected();
+    }, []);
+
     return (
         <View style={commonStyles.container}>
-            <Text>ProfilScreen</Text>
+            <TopBar hideSearchBar={true} />
+            <View style={commonStyles.contentWithTopBar}>
+                <Header label={userProfilName} displayHomeButton={false} />
+                <View style={ProfileStyles.profileContainer}>
+                    <Text style={ProfileStyles.profileSubTitle}>{user?.resources.size} enregistrement(s)</Text>
+                    <Text style={ProfileStyles.profileDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur risus tempus, eleifend sem in, ornare quam. Integer ultrices</Text>
+                    <Text style={[commonStyles.title, ProfileStyles.profileTitle]}>Statistiques</Text>
+                    <Text>Graph : // TODO </Text>
+                </View>
+            </View>
             <NavBar client={client} />
         </View>
     )
---
+++ src/styles/ProfileStyles.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    profileContainer: {
+        padding: 10,
+    },
+    profileTitle: {
+        fontSize: 24,
+        fontWeight: '800',
+        textAlign: 'center',
+        marginVertical: 10,
+    },
+    profileSubTitle: {
+        fontSize: 16,
+        fontWeight: '300',
+        textAlign: 'center',
+        marginVertical: 10,
+    },
+    profileDescription: {
+        fontSize: 12,
+        width: '100%',
+        marginVertical: 10,
+        marginHorizontal: 10,
+        justifyContent: 'center',
+    },
+});
