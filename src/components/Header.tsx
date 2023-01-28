930c957722aae744c4f4370dbeffcfece2835f48
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-28 16:37:13 +0100

    Adding custom properties on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,30 @@ import HomeButton from "./HomeButton";
 
 interface Props {
     label: string;
+    displayHomeButton?: boolean;
 }
 
-export default function Header({label}: Props) {
+export default function Header({ label, displayHomeButton=true }: Props) {
+
+    const getTitleStyle = () => {
+        if (label.length < 10) {
+            return commonStyles.title1;
+        } else if (label.length < 16) {
+            return commonStyles.title2;
+        } else if (label.length < 24) {
+            return commonStyles.title3;
+        }
+        return commonStyles.title;
+    }
 
     return (
         <View style={commonStyles.header}>
-            <HomeButton style={commonStyles.btnHomeBackground} />
-            <Text style={commonStyles.title}>
+            {
+                displayHomeButton ?
+                <HomeButton style={commonStyles.btnHomeBackground} />
+                : null
+            }
+            <Text style={[commonStyles.title, getTitleStyle()]}>
                 { label }
             </Text>
         </View>
