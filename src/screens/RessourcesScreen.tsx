a44320042be6731403e6110ad521bce033755d2b
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-28 12:15:32 +0100

    Adding dynamic resources in resources screen

--- src/screens/RessourcesScreen.tsx
+++ src/screens/RessourcesScreen.tsx
@@ -1,16 +1,42 @@
-import React from "react";
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, Image, SafeAreaView, ScrollView } from 'react-native'
 import { Client } from "rr-apilib";
 import NavBar from "../components/NavBar";
 import commonStyles from "../styles/commonStyles";
+import ButtonShowMoreItems from "../components/buttonShowMoreItems";
+import ResourceCard from "../components/ResourceCard";
 
 export default function RessourcesScreen({ route }: any) {
     const client = route.params as Client;
+    const [showMoreItems, setShowMoreItems] = useState(false);
+    const [resources, setResources] = useState(Array.from(client.resources.cache.values()));
+
+    const onClickShowMoreItems = () => {
+        setShowMoreItems(true);
+    }
 
     return (
-        <View style={commonStyles.container}>
-            <Text>RessourcesScreen</Text>
-            <NavBar client={client}/>
-        </View>
+        <SafeAreaView style={commonStyles.container}>
+            <Image source={require('../assets/rr-logo.png')} style={commonStyles.logo} />
+            <View style={commonStyles.content}>
+                <ScrollView style={commonStyles.resourcesContainer} contentContainerStyle={commonStyles.scrollViewCenter} >
+                    {
+                        resources.map((resource, i) => {
+                            if (!showMoreItems && i < 6) {
+                                return <ResourceCard title={resource.title} user={resource.user?.name} description={resource?.description} key={i}></ResourceCard>
+                            } else if (showMoreItems) {
+                                return <ResourceCard title={resource.title} user={resource.user?.name} description={resource?.description} key={i}></ResourceCard>
+                            }
+                        })
+                    }
+                    {
+                        !showMoreItems ?
+                        <ButtonShowMoreItems callBack={onClickShowMoreItems} />
+                        : null
+                    }
+                </ScrollView>
+            </View>
+            <NavBar client={client} />
+        </SafeAreaView>
     )
 }
