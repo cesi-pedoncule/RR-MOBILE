f70b3491af6d04e9c252b898f98a42051f7d6ee2
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-29 18:42:53 +0100

    Zut j'avais oublié un fichier dans le commit d'avant

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import { Client } from 'rr-apilib';
-import RessourcesScreen from './src/screens/RessourcesScreen';
+import ResourcesScreen from './src/screens/ResourcesScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import ShareResourceScreen from './src/screens/ShareResourceScreen';
 import CategoriesScreen from './src/screens/CategoriesScreen';
@@ -26,7 +26,7 @@ export default function App() {
         <Stack.Screen name="Categories" component={CategoriesScreen} initialParams={client} />
         <Stack.Screen name="Profile" component={ProfileScreen} initialParams={client} />
         <Stack.Screen name="ShareCreate" component={ShareResourceScreen} initialParams={client} />
-        <Stack.Screen name="Ressources" component={RessourcesScreen} initialParams={client} />
+        <Stack.Screen name="Ressources" component={ResourcesScreen} initialParams={client} />
       </Stack.Navigator>
     </NavigationContainer>
   );
