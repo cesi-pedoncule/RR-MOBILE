2c2fbee2d35d57c99e45f7da47ba596e5853489f
HM22-code <H.M-22@outlook.fr>
2023-02-12 17:02:52 +0100

    Adding new component stylesheet

---
+++ src/styles/Component/CommentCardStyles.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    cardBackground: {
+        width: "90%",
+        height: "auto",
+        backgroundColor: '#FFFFFF',
+        borderBottomWidth: 2,
+        borderBottomColor: '#EEEEEE',
+        shadowOffset: {width: 0, height: 4},
+        shadowColor: '#000000',
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        borderRadius: 15,
+        margin: 5,
+        maxHeight: "50vh",
+    },
+    cardUser:{
+        height: "5vh",
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginLeft: 20,
+        marginTop: 5,
+    },
+    cardComment:{
+        height: "inherit",
+        color: '#ACB3BF',
+        fontSize: 13,
+        textAlign: 'justify',
+        margin: "auto",
+        marginBottom: 20,
+        marginLeft: 20,
+        marginRight: 20,
+        left: 0,
+        bottom: 0,
+        minHeight: "15vh",
+        overflow: "scroll",
+    },
+});
