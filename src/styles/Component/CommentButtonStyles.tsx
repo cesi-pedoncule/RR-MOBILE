359fd28ac811566ae31a39a17ef34fc2de1628ee
HM22-code <H.M-22@outlook.fr>
2023-02-20 10:45:11 +0100

    Adding new Button Component stylesheet

---
+++ src/styles/Component/CommentButtonStyles.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    commentBtn:{
+        backgroundColor: 'none',
+    },
+    container:{
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        display:'flex',
+        flexDirection : "row",
+    },
+    numberComment:{
+        marginRight:5,
+    },
+});
