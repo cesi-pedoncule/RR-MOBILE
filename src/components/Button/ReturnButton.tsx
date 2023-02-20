8ceb369af6e2173a8691df08e5e28ceb7a9e076d
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-02-20 10:42:20 +0100

    Using styles file for the Return Button component

--- src/components/Button/ReturnButton.tsx
+++ src/components/Button/ReturnButton.tsx
@@ -3,7 +3,7 @@ import { TouchableHighlight } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
-import commonStyles from '../../styles/commonStyles';
+import ReturnButtonStyles from '../../styles/Component/ReturnButtonStyles';
 
 type ReturnButtonStackParamList = {
     ShareCreate: undefined;
@@ -13,7 +13,7 @@ export default function ReturnButton() {
     const navigation = useNavigation<StackNavigationProp<ReturnButtonStackParamList>>();
 
   return (
-    <TouchableHighlight style={commonStyles.returnBtn} underlayColor="#FFFFFF" onPress={() => navigation.goBack()}>
+    <TouchableHighlight style={ReturnButtonStyles.returnBtn} underlayColor="#FFFFFF" onPress={() => navigation.goBack()}>
       <MaterialCommunityIcons name="arrow-left-top" size={24} color="black" />
     </TouchableHighlight>
   )
---
+++ src/styles/Component/ReturnButtonStyles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    returnBtn:{
+        padding: 10,
+        justifyContent: 'center',
+        textAlign: 'center',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 7,
+        transition: 0.2,
+        marginHorizontal: "6%",
+    },
+
+});
