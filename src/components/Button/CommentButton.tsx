c56307479ae44ad86ed4cfeaabf51f64d97689db
Horziox <horziox.dev@gmail.com>
2023-02-20 11:09:18 +0100

    fix tabs

--- src/components/Button/CommentButton.tsx
+++ src/components/Button/CommentButton.tsx
@@ -1,22 +1,27 @@
-import { View, Text, TouchableHighlight } from 'react-native'
+
 import React from 'react'
-import CommentButtonStyles from '../../styles/Component/CommentButtonStyles';
+import {
+    View,
+    Text,
+    TouchableHighlight
+} from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import CommentButtonStyles from '../../styles/Component/CommentButtonStyles';
 
 interface Props {
-	callBack: ()=>void;
-	commentNumber:number;
-  }
+	callBack: () => void;
+	commentNumber: number;
+}
 
 export default function CommentButton({ callBack, commentNumber }: Props) {
-  return (
-    <View style={CommentButtonStyles.container}>
-      <Text style={CommentButtonStyles.numberComment}>{commentNumber.toString()}</Text>
-			<TouchableHighlight style={CommentButtonStyles.commentBtn} onPress={callBack}>
-				{
+    return (
+        <View style={CommentButtonStyles.container}>
+            <Text style={CommentButtonStyles.numberComment}>{commentNumber.toString()}</Text>
+            <TouchableHighlight style={CommentButtonStyles.commentBtn} onPress={callBack}>
+                {
                     <MaterialCommunityIcons name="comment-outline" size={24} color="black" />
-				}
-			</TouchableHighlight>
-    </View>
-  )
+                }
+            </TouchableHighlight>
+        </View>
+    )
 }
