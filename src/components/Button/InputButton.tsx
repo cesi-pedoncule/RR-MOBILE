4c93d2d8b5d3dae9e72e8259a1dbf6debb12e4a0
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-02-20 10:37:58 +0100

    Fix spaces 2

--- src/components/Button/InputButton.tsx
+++ src/components/Button/InputButton.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 import InputButtonStyles from '../../styles/Component/InputButtonStyles';
 
 interface Props {
-  label: string;
-  callBack: ()=>void;
-  style?: any;
+    label: string;
+    callBack: ()=>void;
+    style?: any;
 }
 
 export default function InputButton({ label, callBack, style}: Props) {
