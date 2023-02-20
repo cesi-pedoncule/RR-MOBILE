5b5bd1f0112108f610c63c69c2c054e26fffa93c
HM22-code <H.M-22@outlook.fr>
2023-02-20 10:43:29 +0100

    Adding new Component CommentButton

---
+++ src/components/Button/CommentButton.tsx
@@ -0,0 +1,22 @@
+import { View, Text, TouchableHighlight } from 'react-native'
+import React from 'react'
+import CommentButtonStyles from '../../styles/Component/CommentButtonStyles';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+interface Props {
+	callBack: ()=>void;
+	commentNumber:number;
+  }
+
+export default function CommentButton({callBack, commentNumber}:Props) {
+  return (
+    <View style={CommentButtonStyles.container}>
+      <Text style={CommentButtonStyles.numberComment}>{commentNumber.toString()}</Text>
+			<TouchableHighlight style={CommentButtonStyles.commentBtn} onPress={callBack}>
+				{
+					<MaterialCommunityIcons name="comment-outline" size={24} color="black" />
+				}
+			</TouchableHighlight>
+    </View>
+  )
+}
--- src/components/Card/ResourceCard.tsx
+++ src/components/Card/ResourceCard.tsx
@@ -4,6 +4,7 @@ import { View, Text } from 'react-native'
 
 import LikeButton from '../Button/LikeButton'
 import ResourceCardStyles from '../../styles/Component/ResourceCardStyles'
+import CommentButton from '../Button/CommentButton'
 
 interface Props {
     resource: Resource;
@@ -13,6 +14,7 @@ export default function ResourceCard({ resource }: Props) {
 
     const [isLikeResource, setIsLikeResource] = useState(false);
     const [numberLikeResource, setNumberLikeResource] = useState(0);
+    const [numberCommentResource, setNumberCommentResource] = useState(0);
 
     const username = resource.user ? `${resource.user.name} ${resource.user.firstname}` : "Utilisateur inconnu";
     // Set the description to "Aucune description fournie" if the description is null or undefined and if the description is longer than 217 characters, cut it and add "..." at the end
@@ -23,6 +25,10 @@ export default function ResourceCard({ resource }: Props) {
         isLikeResource? setNumberLikeResource(numberLikeResource - 1) : setNumberLikeResource(numberLikeResource + 1);
     }
 
+    const onClickComment = () => {
+        alert('TODO: Navigate to DetailResource Screen');
+    }
+
     return (
         <View style={ResourceCardStyles.cardBackground}>
             <View>
@@ -30,6 +36,7 @@ export default function ResourceCard({ resource }: Props) {
                     <Text style={ResourceCardStyles.cardUser}>{username}</Text>
                     <View style={ResourceCardStyles.likeBtn}>
                         <LikeButton callBack={onClickLike} isLike={isLikeResource} likeNumber={numberLikeResource}/>
+                        <CommentButton callBack={onClickComment} commentNumber={numberCommentResource}/>
                     </View>
                 </View>
                 <Text style={ResourceCardStyles.cardTitle}>{resource.title}</Text>
