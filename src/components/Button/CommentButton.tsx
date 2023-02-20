2588d37efd62e5e0379dbe1b8f299d5489a3ced4
HM22-code <H.M-22@outlook.fr>
2023-02-20 11:01:48 +0100

    Change spacing

--- src/components/Button/CommentButton.tsx
+++ src/components/Button/CommentButton.tsx
@@ -11,12 +11,12 @@ interface Props {
 export default function CommentButton({callBack, commentNumber}:Props) {
   return (
     <View style={CommentButtonStyles.container}>
-      <Text style={CommentButtonStyles.numberComment}>{commentNumber.toString()}</Text>
-			<TouchableHighlight style={CommentButtonStyles.commentBtn} onPress={callBack}>
-				{
-					<MaterialCommunityIcons name="comment-outline" size={24} color="black" />
-				}
-			</TouchableHighlight>
+        <Text style={CommentButtonStyles.numberComment}>{commentNumber.toString()}</Text>
+        <TouchableHighlight style={CommentButtonStyles.commentBtn} onPress={callBack}>
+            {
+                <MaterialCommunityIcons name="comment-outline" size={24} color="black" />
+            }
+        </TouchableHighlight>
     </View>
   )
 }
