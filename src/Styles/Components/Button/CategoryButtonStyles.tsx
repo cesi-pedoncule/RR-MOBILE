0a4300ddbc7496fb3a6236dea2b265312bfc9b57
HM22-code <H.M-22@outlook.fr>
2023-02-21 15:01:22 +0100

    Adding new component stylesheet

---
+++ src/Styles/Components/Button/CategoryButtonStyles.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    btnBackground: {
+        width: 78,
+        height: 27,
+        justifyContent: 'center',
+        backgroundColor: '#D9D9D9',
+        borderRadius: 15,
+    },
+    btnText: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        marginVertical: 8,
+    },
+});
