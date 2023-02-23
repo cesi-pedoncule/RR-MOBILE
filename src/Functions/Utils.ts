1dff348c8764be0568d6c600f8212cbc4749f826
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-02-23 13:22:40 +0100

    Update lib

--- src/Functions/Utils.ts
+++ src/Functions/Utils.ts
@@ -3,8 +3,8 @@ import { Resource } from "rr-apilib";
 
 export function likeClickHandle(resource: Resource, isLikeResource: boolean, setIsLikeResource: React.Dispatch<React.SetStateAction<boolean>>, numberLike: number, setNumberLike: React.Dispatch<React.SetStateAction<number>>) {
     if (resource.client.auth.me) {
-        resource.hasLike() ? resource.unlike() : resource.like();
-        setIsLikeResource(!resource.hasLike());
+        resource.isLiked() ? resource.unlike() : resource.like();
+        setIsLikeResource(!resource.isLiked());
         setNumberLike(isLikeResource ? numberLike - 1 : numberLike + 1);
     } else {
         alert("Vous devez être connecté pour liker une ressource");
