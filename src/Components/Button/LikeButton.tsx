b1bc36ea74555afe5da814de4077cc02798238e6
Horziox <horziox.dev@gmail.com>
2023-02-23 09:33:48 +0100

    fixe like/unlike feature on single/double click

--- src/Components/Button/LikeButton.tsx
+++ src/Components/Button/LikeButton.tsx
@@ -1,41 +1,38 @@
+import React from 'react'
+import { Resource } from 'rr-apilib'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import {Text, TouchableHighlight, View } from 'react-native'
-import React, { useEffect } from 'react'
+
+import { likeClickHandle } from '../../Functions/Utils'
 import LikeButtonStyles from '../../Styles/Components/Button/LikeButtonStyles';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Resource } from 'rr-apilib';
 
 interface Props {
 	resource: Resource;
-	isLiked ?: boolean;
+	isLikeResource: boolean;
+	setIsLikeResource: React.Dispatch<React.SetStateAction<boolean>>;
+	numberLike: number;
+	setNumberLike: React.Dispatch<React.SetStateAction<number>>
   }
 
-export default function LikeButton({ resource, isLiked }: Props) {
+export default function LikeButton({ resource, isLikeResource, setIsLikeResource, numberLike, setNumberLike }: Props) {
 
-	const [isLikeResource, setIsLikeResource] = React.useState<boolean>(resource.hasLike() || isLiked ? true : false);
-	const [numberLike, setNumberLike] = React.useState<number>(isLiked ? resource.likes.cache.size + 1 : resource.likes.cache.size);
+	// const [isLikeResource, setIsLikeResource] = React.useState<boolean>(resource.hasLike());
+	// const [numberLike, setNumberLike] = React.useState<number>(resource.likes.cache.size);
 
 	const onClickLike = async () => {
 		if (resource.client.auth.me) {
-			resource.hasLike() || isLiked ? resource.unlike() : resource.like();
-			setIsLikeResource(resource.hasLike() || isLiked ? false : true)
-			isLiked = false;
+			resource.hasLike() ? resource.unlike() : resource.like();
+			setIsLikeResource(!resource.hasLike());
 			setNumberLike(isLikeResource ? numberLike - 1 : numberLike + 1);
 		} else {
 			alert("Vous devez être connecté pour liker une ressource");
 		}
 	}
 
-	useEffect(() => {
-		if (isLiked) {
-			setIsLikeResource(true);
-			isLiked = false;
-		}
-	}, [isLiked]);
-
 	return (
 		<View style={LikeButtonStyles.container}>
-			<Text style={LikeButtonStyles.numberLike}>{ ((isLiked ? 1 : 0) + numberLike).toString()}</Text>
-			<TouchableHighlight style={LikeButtonStyles.likeBtn} onPress={onClickLike} underlayColor={"#F0F0F0"}>
+			<Text style={LikeButtonStyles.numberLike}>{(numberLike).toString()}</Text>
+			<TouchableHighlight style={LikeButtonStyles.likeBtn} onPress={() => likeClickHandle(resource, isLikeResource, setIsLikeResource, numberLike, setNumberLike)} underlayColor={"#F0F0F0"}>
 				<MaterialCommunityIcons name={isLikeResource ? "cards-heart" : "cards-heart-outline" } size={24} color="black" />
 			</TouchableHighlight>
 		</View>
