194b9764b2975e033a2287177229d3395eb6c2a0
Horziox <horziox.dev@gmail.com>
2023-02-23 16:28:42 +0100

    :o

--- src/Functions/Utils.ts
+++ src/Functions/Utils.ts
@@ -3,8 +3,8 @@ import { Resource } from "rr-apilib";
 
 export function likeClickHandle(resource: Resource, isLikeResource: boolean, setIsLikeResource: React.Dispatch<React.SetStateAction<boolean>>, numberLike: number, setNumberLike: React.Dispatch<React.SetStateAction<number>>) {
     if (resource.client.auth.me) {
-        resource.isLiked() ? resource.unlike() : resource.like();
-        setIsLikeResource(!resource.isLiked());
+        isLikeResource ? resource.unlike() : resource.like();
+        setIsLikeResource(!isLikeResource);
         setNumberLike(isLikeResource ? numberLike - 1 : numberLike + 1);
     } else {
         alert("Vous devez être connecté pour liker une ressource");
