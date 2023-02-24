3b910ba80258386b1851f0d48f8e9aea0b3f20ff
Quentin-rslt <quentinr82@protonmail.com>
2023-02-24 15:59:36 +0100

    add style

--- src/Components/Button/IconButton.tsx
+++ src/Components/Button/IconButton.tsx
@@ -8,11 +8,12 @@ interface Props {
     size: number;
     name: any;
     color: string;
+    style?: any;
 }
 
-export default function IconButton({callBack, name, size, color} : Props) {
+export default function IconButton({callBack, name, size, color, style} : Props) {
     return (
-        <TouchableOpacity onPress={callBack} style={deleteButtonStyles.container}>
+        <TouchableOpacity onPress={callBack} style={[deleteButtonStyles.container, style]}>
             <MaterialCommunityIcons name={name} size={size} color={color} />
         </TouchableOpacity>
     )
