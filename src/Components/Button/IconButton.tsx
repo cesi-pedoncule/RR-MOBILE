e5eb5d4958437ac5c065269822038119e969c22c
Quentin-rslt <quentinr82@protonmail.com>
2023-02-24 15:56:08 +0100

    add size

--- src/Components/Button/IconButton.tsx
+++ src/Components/Button/IconButton.tsx
@@ -5,13 +5,14 @@ import deleteButtonStyles from '../../Styles/Components/Button/IconButtonStyles'
 
 interface Props {
     callBack: ()=>void;
+    size: number;
     name: any;
 }
 
-export default function IconButton({callBack, name} : Props) {
+export default function IconButton({callBack, name, size} : Props) {
     return (
         <TouchableOpacity onPress={callBack} style={deleteButtonStyles.container}>
-            <MaterialCommunityIcons name={name} size={24} color="black" />
+            <MaterialCommunityIcons name={name} size={size} color="black" />
         </TouchableOpacity>
     )
 }
