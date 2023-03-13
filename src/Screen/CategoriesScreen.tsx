546bb70f35144f382b2e2906ca3c40bb423a2072
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-03-13 17:32:53 +0100

    Adding hook usage to load categories

--- src/Screen/CategoriesScreen.tsx
+++ src/Screen/CategoriesScreen.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import { View, ScrollView, Text } from 'react-native'
-import { Category, Client } from "rr-apilib";
+import { View, ScrollView, Text, ActivityIndicator } from 'react-native'
+import { Client } from "rr-apilib";
 import CommonStyles from "../Styles/CommonStyles";
 import ButtonShowMoreItems from "../Components/Button/ButtonShowMoreItems";
 import TopBar from "../Components/Input/TopBar";
 import CategoryCard from "../Components/Card/CategoryCard";
 import CategoryStyles from "../Styles/Screen/CategoryStyles";
+import useCategories from "../Hooks/useCategories";
 
 export default function CategoriesScreen({ route, navigation }: any) {
     const client = route.params as Client;
-    const [showMoreItems, setShowMoreItems] = useState<boolean>(false);
-    const [categories, setCategories] = useState<Category[]>(Array.from(client.categories.cache.values()));
+
+    const { categories, setCategories, loading } = useCategories({ client });
+    const [ showMoreItems, setShowMoreItems ] = useState<boolean>(false);
 
     const onClickShowMoreItems = () => {
         setShowMoreItems(true);
@@ -26,28 +28,31 @@ export default function CategoriesScreen({ route, navigation }: any) {
     return (
         <View style={CommonStyles.container}>
             <TopBar onChangeSearch={handleChangeSearch} navigation={navigation} />
-            <View style={CommonStyles.content}> 
-                <ScrollView style={CommonStyles.scrollViewWithNavBar}>
-                    <View style={CategoryStyles.categoriesContainer}>
-                        {
-                            categories.map((category, i) => {
-                                if ((!showMoreItems && i < 6) || showMoreItems) {
-                                    return <CategoryCard category={category} key={i} />
-                                }
-                            })
-                        }
-                        {
-                            categories.length == 0 && <Text style={CommonStyles.textEmptyResult}> Aucune catégorie n'a été trouvée. </Text>
-                        }
-                    </View>
-                    <View style={CategoryStyles.showMoreItemsContainer}>
-                    {
-                        !showMoreItems && categories.length >= 6 && (
-                            <ButtonShowMoreItems callBack={onClickShowMoreItems} />
-                        )
-                    }
-                    </View>
-                </ScrollView>
+            <View style={CommonStyles.content}>
+                {
+                    loading ?  <ActivityIndicator size="large" color="#0000ff" style={CommonStyles.loader} /> :
+                    <ScrollView style={CommonStyles.scrollViewWithNavBar}>
+                        <View style={CategoryStyles.categoriesContainer}>
+                            {
+                                categories.map((category, i) => {
+                                    if ((!showMoreItems && i < 6) || showMoreItems) {
+                                        return <CategoryCard category={category} key={i} />
+                                    }
+                                })
+                            }
+                            {
+                                categories.length == 0 && <Text style={CommonStyles.textEmptyResult}> Aucune catégorie n'a été trouvée. </Text>
+                            }
+                        </View>
+                        <View style={CategoryStyles.showMoreItemsContainer}>
+                            {
+                                !showMoreItems && categories.length >= 6 && (
+                                    <ButtonShowMoreItems callBack={onClickShowMoreItems} />
+                                )
+                            }
+                        </View>
+                    </ScrollView>
+                }
             </View>
         </View>
     )
