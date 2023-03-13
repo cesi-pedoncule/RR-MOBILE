4a0b1b40cdcbe3a9728dcadad85939c4601a4f92
Quentin-rslt <quentinr82@protonmail.com>
2023-03-13 15:54:11 +0100

    Add shadow

--- src/Styles/Components/Button/InputButtonStyles.tsx
+++ src/Styles/Components/Button/InputButtonStyles.tsx
@@ -4,9 +4,13 @@ import { COLORS } from "../../Colors";
 export default StyleSheet.create({
     btnBackground: {
         width: 126,
-        height: 34,
+        height: 50,
         justifyContent: 'center',
         backgroundColor: COLORS.AccentColor,
+        shadowColor: COLORS.Black,
+        shadowOpacity: 0.3,
+        shadowOffset: { width: 4, height: 4},
+        elevation: 2,
         borderRadius: 15,
         transition: 0.2,
     },
