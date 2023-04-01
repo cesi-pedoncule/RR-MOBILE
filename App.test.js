f43f436a0b753195644a19884f1039f8dd81485a
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-04-01 15:03:05 +0200

    Adding jest tests

---
+++ App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
+jest.useFakeTimers();
+
+describe('<App />', () => {
+
+    it('renders correctly', () => {
+        const tree = renderer
+            .create(<App />)
+            .toJSON();
+            
+    });
+    
+});
