f154334e36303b36113a3939dc6089d178691aca
Quentin-rslt <quentinr82@protonmail.com>
2023-04-09 11:15:10 +0200

    Remove unused style

--- src/Components/Button/StateButton.tsx
+++ src/Components/Button/StateButton.tsx
@@ -1,6 +1,5 @@
 import { View } from 'react-native'
 import React from 'react'
-import StateButtonStyles from '../../Styles/Components/Button/StateButtonStyles'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Resource } from 'rr-apilib';
 import { COLORS } from '../../Styles/Colors';
@@ -11,18 +10,18 @@ interface Props {
 
 export default function StateButton( {resource} : Props) {
     return (
-        <View style={StateButtonStyles.container}>
+        <View>
             {
                 resource.validations.getLastValidationState()?.state.toString() == "validated" &&
-                    <MaterialCommunityIcons style={StateButtonStyles.icon} name="check-decagram-outline" size={24} color={COLORS.AccentColor} />
+                    <MaterialCommunityIcons name="check-decagram-outline" size={24} color={COLORS.AccentColor} />
             }
             {
                 resource.validations.getLastValidationState()?.state.toString() == "pending" &&
-                    <MaterialCommunityIcons style={StateButtonStyles.icon} name="clock-outline" size={24} color={COLORS.Orange} />
+                    <MaterialCommunityIcons name="clock-outline" size={24} color={COLORS.Orange} />
             }
             {
                 resource.validations.getLastValidationState()?.state.toString() == "rejected" &&
-                    <MaterialCommunityIcons style={StateButtonStyles.icon} name="cancel" size={24} color={COLORS.Red} />
+                    <MaterialCommunityIcons name="cancel" size={24} color={COLORS.Red} />
             }
         </View>
     )
